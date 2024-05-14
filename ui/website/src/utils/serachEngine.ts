0af0863f8fc5c5bd8ec76451a6542a3a4d889e8d
wushuangshangjiang <emeanshahabul8386837@gmail.com>
2024-05-14 08:45:25 +0800

    update

--- ui/website/src/utils/serachEngine.ts
+++ ui/website/src/utils/serachEngine.ts
@@ -2,11 +2,11 @@ export const generateSearchEngineCard = (searchString: string) => {
   if (!searchString.trim()) return [];
   const result = [
     {
-      name: "使用百度搜索",
-      url: searchBaidu(searchString),
-      desc: `在百度中搜索 「${searchString}」`,
+      name: "使用 Google 搜索",
+      url: searchGoogle(searchString),
+      desc: `在 Google 中搜索 「${searchString}」`,
       id: 8800880001,
-      logo: "baidu.ico",
+      logo: "google.ico",
       hide: false
     },
     {
@@ -18,18 +18,16 @@ export const generateSearchEngineCard = (searchString: string) => {
       hide: false
     },
     {
-      name: "使用 Google 搜索",
-      url: searchGoogle(searchString),
-      desc: `在 Google 中搜索 「${searchString}」`,
+      name: "使用百度搜索",
+      url: searchBaidu(searchString),
+      desc: `在百度中搜索 「${searchString}」`,
       id: 8800880003,
-      logo: "google.ico",
+      logo: "baidu.ico",
       hide: false
     }
   ]
   return result;
 }
-
-
 
 const searchBaidu = (q: string) => {
   return `https://www.baidu.com/s?wd=${q}`
